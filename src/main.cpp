14c5153f3eb70ffda91503fb468c0d09d101b9ec
JonathanMercado16 <160290153+JonathanMercado16@users.noreply.github.com>
2024-04-23 01:44:44 +0000

    Version 2

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,29 @@
 #include <thread> //hilos de procesos
 #include <fstream> //flujos de archivos
 #include <string> //control de cadena 
+#include <Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    fstream archivo;
-    archivo.open("./assets/images/moto.txt");
-    string moto;
-
-    while (archivo.good())
-    {
-        archivo >> moto;
-    }
-    
-    archivo.close();
-
-
+    Dibujo moto("./assets/images/moto.txt");
+    Dibujo bloque("./assets/images/bloque.txt");
+//----------------------------------------------------------------------------------------------------
     int fotograma = 0;
     while (true)
     {
         this_thread::sleep_for(0.1s);
         fotograma++;
-        Element personaje = spinner(21,fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
-        Element eMoto = text(moto);
-        Element dibujo = hbox ({personaje,eMoto});
+        Element personaje = spinner(21,fotograma) | bold | color(Color::White) | bgcolor(Color::Red);
+        Element dibujo = hbox ({personaje,moto.GetElement(),bloque.GetElement()}); 
 
         Screen pantalla = Screen::Create(Dimension::Full());
         Render (pantalla,dibujo);
         pantalla.Print();
         cout<<pantalla.ResetPosition();
     }
-    
-    
 
 return EXIT_SUCCESS;
 }
